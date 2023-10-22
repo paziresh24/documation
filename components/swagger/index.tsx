143525aac4126523cbbe1e019e02b9244da9e688
Amirhossein <dev.amirhbeigi@gmail.com>
2023-10-22 10:40:59 +0330

    chore: change package manager to npm

--- components/swagger/index.tsx
+++ components/swagger/index.tsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 const SwaggerUI = dynamic<{
   url: string;
   docExpansion: string;
-}>(import("swagger-ui-react"), { ssr: false });
+}>(() => import("swagger-ui-react"), { ssr: false });
 
 const SwaggerWrapper = () => (
   <div dir="ltr">
